d7015d735c43487cac4b50d152f7bae09a36b323
gmlgml5023 <gmlgml5023@naver.com>
2023-09-18 10:38:03 +0900

    삼성전가 주가, 뉴스 분석 프로젝트_주가데이터 크롤링

--- project_1.py
+++ project_1.py
@@ -1,14 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime
 
-URL = 'https://finance.naver.com/item/sise.nhn?code=005930'
+list_date = []
+list_price = []
 
-res = requests.get(URL)
-bs = BeautifulSoup(res.text, 'html.parser')
+for i in range(2, 5, 1):
+    
+    URL = 'https://finance.naver.com/item/sise_day.naver?code=005930&'
+    URL = URL+'page='+str(i)
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'}
 
-result = bs.select('td.num > span.tah.p11')
+    res = requests.get(URL, headers = headers)
+    bs = BeautifulSoup(res.text, 'html.parser')
+
+    result_date = bs.select('table.type2 tr > td > span.tah.p10.gray03')
+    # print(result_date)
+    result_price = bs.select('table.type2 tr > td.num > span.tah.p11')
+    # print(result_price[0])
 
 
-for data in result:
-    print(data.text.strip())
+    for data1 in result_date:
+        date = data1.text
+        list_date.append(date)
+    # print(list_date)
+    
+    for i in range(0, len(result_price), 6):
+        price = result_price[i].text
+        list_price.append(price)
+    # print(list_price)
 
+stock = pd.DataFrame({'Date' : list_date, 'Price' : list_price})
+stock['Date'] = pd.to_datetime(stock.Date)
+stock = stock[stock['Date'].dt.month == 8]
+
+stock.to_csv('stock_info.csv', index = False)
